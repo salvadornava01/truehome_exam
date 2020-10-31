764df9eaa1f00f3434e7be44ee59f7d79c3caa33
Salvador Nava <snava@e-bitware.com.mx>
2020-10-30 21:29:00 -0600

    Cambio debug

--- src/truehome/settings.py
+++ src/truehome/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!0*e72h3%2axb2r61hwqqpl)%ad_sd25+mt++5xhuqltj65ja5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = ['truehome-test.herokuapp.com', 'localhost']
 
