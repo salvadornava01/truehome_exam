c3a843a6b7a425fe01553dddb61c60d9c856fe54
Salvador Nava <snava@e-bitware.com.mx>
2020-10-30 21:38:55 -0600

    install django heroku

--- src/truehome/settings.py
+++ src/truehome/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django_heroku
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -122,3 +124,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+django_heroku.settings(locals())
